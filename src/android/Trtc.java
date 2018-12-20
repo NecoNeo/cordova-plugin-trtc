99bcc630585a46b936aced9d4de9618c66c56af8
NecoNeo <neo2314@qq.com>
2018-12-20 18:48:47 +0800

    修改 未跑通

--- src/android/Trtc.java
+++ src/android/Trtc.java
@@ -19,19 +19,22 @@ import org.json.JSONArray;
 import org.json.JSONException;
 // import org.json.JSONObject;
 
+import com.tencent.qcloud.videocall.ui.CreateActivity;
+
 
 public class Trtc extends CordovaPlugin {
 
+    static final String ACTION_SHOW_CREATE_ACTIVITY = "showCreateActivity"; // for test
     static final String ACTION_JOIN_CHANNEL = "joinChannel";
 
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
         Activity activity = this.cordova.getActivity();
 
-        // if (ACTION_JOIN_CHANNEL.equals(action)) {
-        //     Intent intent = new Intent(activity, ChatRoomActivity.class);
-        //     cordova.startActivityForResult(this, intent, 666);
-        // }
+        if (ACTION_SHOW_CREATE_ACTIVITY.equals(action)) {
+            Intent intent = new Intent(activity, CreateActivity.class);
+            cordova.startActivityForResult(this, intent, 666);
+        }
 
         return false;
     }
--- www/trtc.js
+++ www/trtc.js
@@ -2,13 +2,12 @@ var exec = require('cordova/exec');
 
 function Trtc() {}
 
-Trtc.prototype.hello = function() {
-    window.alert('h');
-    console.log('hola');
-};
-
 Trtc.prototype.joinChannel = function(channelId, uid) {
     // exec(function(result) {}, function(err) {}, 'Agora', 'joinChannel', [channelId, uid]);
 };
+
+Trtc.prototype.showCreateActivity = function() {
+    exec(function(result) {}, function(err) {}, 'Trtc', 'showCreateActivity', []);
+}
 
 module.exports = new Trtc();
