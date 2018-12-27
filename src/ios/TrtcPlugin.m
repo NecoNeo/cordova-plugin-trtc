fffef1d192ef2743427ecc6e582322e661e41883
NecoNeo <neo2314@qq.com>
2018-12-27 17:06:46 +0800

    entrance for test

--- src/ios/TrtcPlugin.m
+++ src/ios/TrtcPlugin.m
@@ -5,6 +5,12 @@
 //
 
 #import "TrtcPlugin.h"
+#import "TCLiveRequestManager.h"
+#import <ILiveSDK/ILiveSDK.h>
+#import "TCLiveJoinRoomViewController.h"
+#import <ILiveSDK/ILiveLoginManager.h>
+#import "UIToastView.h"
+#import <ILiveLogReport/ILiveLogReport.h>
 
 @interface TrtcPlugin()
 {}
@@ -13,7 +19,31 @@
 @implementation TrtcPlugin
 
 - (void) showCreatePage:(CDVInvokedUrlCommand*)command {
-    NSLog(@"sssssssssssssssssssssdk inited");
+    NSLog(@"showCreatePage");
+    [[TCLiveRequestManager getInstance] requestLoginInfo:^(int code) {
+        if (code == 0) {
+            dispatch_async(dispatch_get_main_queue(), ^{
+                int retCode = [[ILiveSDK getInstance] initSdk:[TCLiveRequestManager getInstance].sdkAppID accountType:[TCLiveRequestManager getInstance].accountType];
+                NSLog(@"initSdk success %d",retCode);
+                if (retCode == 0) {
+                    NSLog(@"userId & sig:");
+                    NSLog(@"%@", [TCLiveRequestManager getInstance].userID);
+                    NSLog(@"%@", [TCLiveRequestManager getInstance].userSig);
+                    [[ILiveLoginManager getInstance] iLiveLogin:[TCLiveRequestManager getInstance].userID sig:[TCLiveRequestManager getInstance].userSig succ:^{
+                        NSLog(@"-----> login  succ");
+                        [[UIToastView getInstance] showToastWithMessage:@"登录成功" toastMode:UIToastShowMode_Succ];
+                    } failed:^(NSString *module, int errId, NSString *errMsg) {
+                        NSLog(@"-----> login  fail,%@ %d %@",module, errId, errMsg);
+                        [[UIToastView getInstance] showToastWithMessage:@"登录失败" toastMode:UIToastShowMode_fail];
+                    }];
+                }
+            });
+        }
+    }];
+    TCLiveJoinRoomViewController *vc = [TCLiveJoinRoomViewController new];
+//    vc.defaultRoomId = self.defaultRoomId;
+    UINavigationController *nav = [[UINavigationController alloc] initWithRootViewController:vc];
+    [self.viewController presentViewController:nav animated:YES completion:nil];
 }
 
 @end
