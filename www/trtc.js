e310422dd1c84713960dd41401ef354805576034
NecoNeo <neo2314@qq.com>
2018-12-28 10:38:02 +0800

    fix

--- www/trtc.js
+++ www/trtc.js
@@ -2,8 +2,8 @@ var exec = require('cordova/exec');
 
 function Trtc() {}
 
-Trtc.prototype.joinChannel = function(channelId, uid) {
-    // exec(function(result) {}, function(err) {}, 'Agora', 'joinChannel', [channelId, uid]);
+Trtc.prototype.joinChannel = function(params) {
+    // not to be implemented
 };
 
 Trtc.prototype.showCreatePage = function() {
